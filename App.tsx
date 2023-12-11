baf9e0eb52f71a5b5168b3517ada917c09cba4d0
pato <paguirre@utem.cl>
2023-12-11 12:41:53 -0300

    modificacion splash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import SignInScreen from './src/screens/SignInScreen';
-import Loader from "./src/components/Loader";
+import { View, Text, Image } from 'react-native';
+import splashImage from './assets/logo2.png';
 
 type RootStackParamList = {
   Login: undefined;
@@ -20,6 +21,25 @@ const stackScreenOptions = {
 };
 
 const App  = () => {
+
+  const [isSplashScreenVisible, setIsSplashScreenVisible] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsSplashScreenVisible(true);
+        }, 3000); // 3 segundos
+    }, []);
+
+    if (isSplashScreenVisible) {
+        return (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+          <Image source={splashImage} style={{ width: 250, height: 250 }} />
+          
+          <Text style={{ color:'green',marginBottom:100, fontSize:40, marginTop:30}}>Nombre APP </Text>
+          <Text>Cargando...</Text>
+      </View>
+        );
+    }
   return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login" screenOptions={stackScreenOptions}>
